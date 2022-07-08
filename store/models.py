8c260bc293a12e72225fead61a317f5d514130ce
aditya dangi <adityadangi1@gmail.com>
2022-07-08 15:00:12 +0530

    added product manager to handle is_active filter

--- store/models.py
+++ store/models.py
@@ -1,6 +1,13 @@
+from itertools import product
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+
+
+class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
 
 
 class Category(models.Model):
@@ -34,6 +41,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'Products'
