282c3c1526bb4d80e21dfe126dc5c71399f66614
aditya dangi <adityadangi1@gmail.com>
2022-07-08 15:13:40 +0530

    changed name of a view, edit references to it

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def all_products(request):
+def product_all(request):
 
     products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
