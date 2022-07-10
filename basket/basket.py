3dcc20e06c0f3cd05b6b5d3299d7c73d02bac683
aditya dangi <adityadangi1@gmail.com>
2022-07-11 02:54:44 +0530

    added delete , update, subtotal functionality

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from store.models import Product
+
+
 class Basket():
     """
     A base Basket class, providing some default behaviors that
@@ -18,17 +21,53 @@ class Basket():
         """
         Adding and Updating the users basket session data
         """
-        product_id = product.id
+        product_id = str(product.id)
 
-        if product_id not in self.basket:
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = product_qty
+        else:
             self.basket[product_id] = {'price': float(product.price),
                                        'qty': int(product_qty)}
 
+        self.save()
+
+    def update(self, product_id, product_qty):
+        print(product_id, product_qty)
+        product_id = str(product_id)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = product_qty
+        self. save()
+
+    def delete(self, product_id):
+        
+        if str(product_id) in self.basket:
+            del self.basket[str(product_id)]
+        self.save()
+
+    def save(self):
+
         self.session.modified = True
 
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database and return products
+        """
+        products_ids = self.basket.keys()
+        products = Product.products.filter(id__in=products_ids)
+        basket = self.basket.copy()
+        
+        for product in products:
+            basket[str(product.id)]['product'] = product
+        
+        for item in basket.values():
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     def __len__(self):
         """
         Get the basket data and count the quantity of items
         """
         return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(item['price'] * item['qty'] for item in self.basket.values())
--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from store.models import Product
@@ -5,7 +6,8 @@ from .basket import Basket
 
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
 
 
 def basket_add(request):
@@ -24,4 +26,32 @@ def basket_add(request):
         basket_qty = basket.__len__()
         response = JsonResponse({'qty': basket_qty})
 
+        return response
+
+def basket_update(request):
+
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(float(request.POST.get('productid')))
+        product_qty = int(float(request.POST.get('productqty')))
+
+        basket.update(product_id, product_qty)
+
+        basket_qty = basket.__len__()
+        basket_total = basket.get_total_price()
+        response = JsonResponse({'qty': basket_qty, 'subtotal': basket_total})
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(float(request.POST.get('productid')))
+        basket.delete(product_id)
+
+        basket_qty = basket.__len__()
+        basket_total = basket.get_total_price()
+        response = JsonResponse({'qty': basket_qty, 'subtotal': basket_total})
         return response
