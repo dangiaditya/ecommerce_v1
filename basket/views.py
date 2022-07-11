c46b40330334a7f476a7ed05972a3ba74cd67c11
aditya dangi <adityadangi1@gmail.com>
2022-07-12 01:06:54 +0530

    added session to view test

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,10 @@
 from urllib import response
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+
 from store.models import Product
+
 from .basket import Basket
 
 
